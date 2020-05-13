d5a0d7c466a3e0a4bbcda8bdefbe603c006d3fe7
Sahana-Lawrence <sahana.lawrence@mths.ca>
2020-05-13 15:56:54 +0000

    updated files

--- index.js
+++ index.js
@@ -1,28 +1,25 @@
 // JavaScript File
 
-// random value generated 
-var y = Math.floor(Math.random() * 6 + 1); 
-  
-// counting the number of guesses 
-// made for correct Guess 
-var guess = 1; 
-  
-document.getElementById("submitguess").onclick = function(){ 
-  
-// number guessed by user      
-var x = document.getElementById("number").value; 
-
-if(x == y) 
-{     
-  alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guess + " GUESS "); 
-} 
-else if(x > y) /* if guessed number is greater than actual number*/ 
-{     
-  guess++; 
-  alert("OOPS SORRY!! TRY A SMALLER NUMBER"); 
-} 
+// random value generated
+var y = Math.floor(Math.random() * 6) + 1;
+// counting the number of guesses
+// made for correct Guess
+var guesses = 1;
+document.getElementById("submitguess").onclick = function() {
+// number guessed by user
+var x = document.getElementById("number").value;
+if(x == y)
+{
+  alert("CONGRATULATIONS!!! YOU GUESSED IT RIGHT IN " + guesses + " GUESS ");
+}
+else if(x > y) /* if guessed number is greater than actual number*/
+{
+  guesses++;
+  alert("OOPS SORRY!! TRY A SMALLER NUMBER");
+}
 else
-{ 
-  guess++; 
-  alert("OOPS SORRY!! TRY A BIGGER NUMBER") 
+{
+  guesses++;
+  alert("OOPS SORRY!! TRY A BIGGER NUMBER");
+}
 }
